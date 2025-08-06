5a95eb5a1b1b1071505f9abf6885715310895ec1
Baque2005 <155196295+Baque2005@users.noreply.github.com>
2025-08-06 13:05:36 -0500

    Mensaje aquí

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ if (!fs.existsSync(uploadDir)) {
 }
 
 // Middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 app.use(session({
   store: new PgSession({
